7899529042cf6a47600f9271bb91f12bc1b50532
mackenzieporupsky <mack@porupsky.net>
2020-09-23 19:56:03 -0500

    forms 1-3
    forms 1-3 run correctly

---
+++ forms/calc/calc.js
@@ -0,0 +1,12 @@
+/*
+function calculate(num1, num2) {
+  let summedNumbers = (num1 + num2)
+  return summedNumbers
+}
+
+let number1 = Number(prompt('Please enter a number'))
+let number2 = Number(prompt('Please enter a second number'))
+
+let userAnswer = calculate(number1, number2)
+console.log(`The sum of ${number1} and ${number2} is ${userAnswer}`)
+*/
---
+++ forms/while/while.js
@@ -0,0 +1,14 @@
+/*
+let userChoice = confirm('Would you like to run the loop?')
+let i = 1
+
+while (userChoice == true) {
+  alert('This is loop iteration ' + i)
+  i++
+  userChoice = confirm('Would you like to run the loop again?')
+    if (userChoice == false) {
+    alert('The loop is now done')
+  }
+} 
+*/
+
